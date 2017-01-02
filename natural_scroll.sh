0f102479e184e996849d5a0ff679b6b2b0931b63
Johnny Brown <jbrown@luc.id>
2017-01-02 10:56:23 -0600

    some updates

--- natural_scroll.sh
+++ natural_scroll.sh
@@ -8,10 +8,6 @@
 # Short-Description: Start daemon at boot time
 # Description:       Enable service provided by daemon.
 ### END INIT INFO
-xinput set-prop 12 "Synaptics Scrolling Distance" -235 -235
-xinput set-prop 13 "Synaptics Scrolling Distance" -235 -23
-xinput set-prop 14 "Synaptics Scrolling Distance" -235 -235
-xinput set-prop 15 "Synaptics Scrolling Distance" -235 -235
-touch /home/jb/Desktop/natscroll
-nautilus -q
-nautilus -n &
+xinput set-prop 10 271 -1 -1 -1
+gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
+xinput set-prop 13 302 -114 -114
